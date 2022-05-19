4400e1b913501695ae6aadd834ce8bc024ba40cf
Yusuke <feelbzz2116@yahoo.co.jp>
2022-05-19 17:05:38 +0900

    fist commit

---
+++ Shift_Work/src/main/java/com/example/demo/ShiftWorkApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiftWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiftWorkApplication.class, args);
+	}
+
+}
